4c01b9bf97ba25189302fa38dea412e32f84de5d
Grayson Shogan <graysontoddshogan@gmail.com>
2024-02-20 12:45:06 -0800

    Done

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import axios from 'axios';
-import { resetVisualizationQuery } from '../../../state/actionCreators';
 import CitizenshipMapAll from './Graphs/CitizenshipMapAll';
 import CitizenshipMapSingleOffice from './Graphs/CitizenshipMapSingleOffice';
 import TimeSeriesAll from './Graphs/TimeSeriesAll';
@@ -10,41 +8,103 @@ import OfficeHeatMap from './Graphs/OfficeHeatMap';
 import TimeSeriesSingleOffice from './Graphs/TimeSeriesSingleOffice';
 import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
-import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
+import axios from 'axios';
+import { resetVisualizationQuery } from '../../../state/actionCreators';
 import { colors } from '../../../styles/data_vis_colors';
+import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
-const GraphWrapper = ({ set_view, dispatch }) => {
-  const { office, view = 'time-series' } = useParams();
-  const [apiData, setApiData] = useState(null);
+const { background_color } = colors;
+function GraphWrapper(props) {
+  const { set_view, dispatch } = props;
+  let { office, view } = useParams();
 
-  useEffect(() => {
-    fetchApiData();
-  }, [view, office]);
+  if (!view) {
+    set_view('time-series');
+    view = 'time-series';
+  }
 
-  const fetchApiData = async () => {
-    const fetchData = async endpoint =>
-      axios.get(`https://hrf-asylum-be-b.herokuapp.com/cases/${endpoint}`);
-    try {
-      const [fiscalData, citizenshipData] = await Promise.all([
-        fetchData('fiscalSummary'),
-        fetchData('citizenshipSummary'),
-      ]);
-      setApiData({
-        fiscalData: fiscalData.data,
-        citizenshipData: citizenshipData.data,
-      });
-    } catch (error) {
-      console.error('Failed to fetch data:', error);
-      setApiData(null);
+  let map_to_render;
+
+  if (!office) {
+    switch (view) {
+      case 'time-series':
+        map_to_render = <TimeSeriesAll />;
+        break;
+      case 'office-heat-map':
+        map_to_render = <OfficeHeatMap />;
+        break;
+      case 'citizenship':
+        map_to_render = <CitizenshipMapAll />;
+        break;
+      default:
+        break;
     }
-  };
+  } else {
+    switch (view) {
+      case 'time-series':
+        map_to_render = <TimeSeriesSingleOffice office={office} />;
+        break;
+      case 'citizenship':
+        map_to_render = <CitizenshipMapSingleOffice office={office} />;
+        break;
+      default:
+        break;
+    }
+  }
 
-  const MapComponent =
-    {
-      'time-series': office ? TimeSeriesSingleOffice : TimeSeriesAll,
-      'office-heat-map': OfficeHeatMap,
-      citizenship: office ? CitizenshipMapSingleOffice : CitizenshipMapAll,
-    }[view] || TimeSeriesAll;
+  async function updateStateWithNewData(
+    years,
+    view,
+    office,
+    stateSettingCallback
+  ) {
+    if (office === 'all' || !office) {
+      try {
+        const [fiscalRes, citizenshipRes] = await Promise.all([
+          axios.get(
+            'https://hrf-asylum-be-b.herokuapp.com/cases/fiscalSummary'
+          ),
+          axios.get(
+            'https://hrf-asylum-be-b.herokuapp.com/cases/citizenshipSummary'
+          ),
+        ]);
+
+        const data = [fiscalRes.data, {}];
+        data[0].citizenshipResults = citizenshipRes.data;
+
+        console.log(data);
+        stateSettingCallback(view, office, data);
+      } catch (err) {
+        console.log(err);
+      }
+    } else {
+      let data = [];
+
+      if (view === 'time-series' || view === 'office-heat-map') {
+        try {
+          const res = await axios.get(
+            'https://hrf-asylum-be-b.herokuapp.com/cases/fiscalSummary',
+            {
+              params: {
+                from: years[0],
+                to: years[1],
+                office: office,
+              },
+            }
+          );
+
+          data[0] = res.data;
+          stateSettingCallback(view, office, data);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    }
+  }
+
+  const clearQuery = (view, office) => {
+    dispatch(resetVisualizationQuery(view, office));
+  };
 
   return (
     <div
@@ -54,11 +114,11 @@ const GraphWrapper = ({ set_view, dispatch }) => {
         alignItems: 'flex-start',
         justifyContent: 'center',
         minHeight: '50px',
-        backgroundColor: colors.background_color,
+        backgroundColor: background_color,
       }}
     >
       <ScrollToTopOnMount />
-      <MapComponent data={apiData} office={office} />
+      {map_to_render}
       <div
         className="user-input-sidebar-container"
         style={{
@@ -73,12 +133,12 @@ const GraphWrapper = ({ set_view, dispatch }) => {
         <YearLimitsSelect
           view={view}
           office={office}
-          clearQuery={() => dispatch(resetVisualizationQuery(view, office))}
-          updateStateWithNewData={fetchApiData}
+          clearQuery={clearQuery}
+          updateStateWithNewData={updateStateWithNewData}
         />
       </div>
     </div>
   );
-};
+}
 
 export default connect()(GraphWrapper);
